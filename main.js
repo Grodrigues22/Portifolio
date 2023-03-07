07181f39a1431bd88358cf727d3a8ae8e60a1d9e
Grodrigues22 <gmrodrigues98@gmail.com>
2023-03-06 23:21:12 -0300

    js do menu com direcionamentos para as paginas

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+/* abre e fecha menu quando clica no icone */
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/* quando cicar o icone do menu sair dele e ir a pagina */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
